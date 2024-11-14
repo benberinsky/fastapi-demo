7ec2e7920d1081fc1c060237ddc0d08d25be1cdc
benberinsky <tfu5hw@virginia.edu>
2024-11-14 17:31:12 -0500

    updated main.py

--- app/main.py
+++ app/main.py
@@ -1,40 +1,54 @@
 #!/usr/bin/env python3
-from fastapi import Request, FastAPI
+from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
-import pandas as pd
+import mysql.connector
+from mysql.connector import Error
 import json
 import os
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
-@app.get("/")  # zone apex
-def zone_apex():
-<<<<<<< HEAD
-    return {"Hello": "Hello API", "album_endpoint":"/albums","static_endpoint":"/static"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/multiply/{c}/{d}")
-def multiply(c: int, d: int):
-    return {product: c*d}
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "tfu5hw"
 
-@app.get("/customer/{idx}")
-def get_customer(idx: int):
-    #Read in data from CSV file
-    df = pd.read_csv("../customers.csv")
-    #Filter data based on the index
-    customer = df.iloc[idx]
-    return customer.to_dict()
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
 
-@app.post("/get_payload")
-async def get_payload(request:Request):
-    response = request.json()
-    geo = response.get("geo")
-    url = "https://maps.google.com/?q={geo}".format(geo=geo)
-    #return await request.json
-=======
-    return {"What's up": "What's up API", "album_endpoint":"/albums","static_endpoint":"/static"}
-    return {"How are you doing": "I am well", "album_endpoint":"/albums","static_endpoint":"/static"}
-    return {"Hey there boss": "Hey there API", "album_endpoint":"/albums","static_endpoint":"/static"}
-    return {"I hope you're happy": "I am!", "album_endpoint":"/albums","static_endpoint":"/static"}
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
 
->>>>>>> 9e14f30f15847878f14d0dd3f68e66a38bc632d7
+@app.get('/songs')
+def get_songs():
+    query = "SELECT songs.id, songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre AS genre FROM songs JOIN genres ON songs.genre = genres.genreid ORDER BY songs.id;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
